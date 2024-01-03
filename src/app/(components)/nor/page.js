094432e4f5162b180ef6b62bd0a3569427c57b9e
Ankit Khanal <72298204+khanalankitt@users.noreply.github.com>
2024-01-03 19:25:14 +0545

    Update page.js

--- src/app/(components)/nor/page.js
+++ src/app/(components)/nor/page.js
@@ -10,8 +10,8 @@ const details = {
     A NOR gate is another basic digital logic gate,
      standing for "NOT-OR." It produces an output that
       is the complement of the logical OR operation.
-       In simpler terms, a NOR gate produces a low or 
-       false output only when both of its inputs are 
+       In simpler terms, a NOR gate produces a high or 
+       true output only when both of its inputs are 
        low or false;
      otherwise, the output is high or true.`,
      symbol:"(A+B)'"
